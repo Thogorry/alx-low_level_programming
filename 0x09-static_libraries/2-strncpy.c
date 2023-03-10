150aaa75d65702043b9c4ddf785b39796d8f609e
Loise Mwai <lomwai@gmail.com>
2023-03-09 22:59:39 -0800

    Static libraries

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return:dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	(void)dest;
+	(void)src;
+	(void)n;
+	return (dest);
+}
